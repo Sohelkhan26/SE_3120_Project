530aa4d490d881c4dbe34c750a7609ce1083ec8f
Md Shahruk Khan Sohel <darkhorse007d@gmail.com>
2024-04-25 10:35:06 +0600

    Login , SignUp activity added , Singleton Class added.

---
+++ app/src/main/java/com/example/se_3120_project/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.se_3120_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this , LogIn.class);
+        startActivity(intent);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SE_3120_Project"
+include(":app")
+ 
